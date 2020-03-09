56db16fbf10e1e7b422c9d2a56d845ddce14e172
Milagros Lucero Jantus Pereira <milagrosj325@alu.bue.edu.ar>
2020-03-09 19:17:27 -0300

    adicion de la query para insertar datos en PRODUCTOS

---
+++ Query de insercion de productos en DB bety_market.sql
@@ -0,0 +1,9 @@
+INSERT INTO bety_market.producto (precio, nombre, url_imagen)
+VALUES (750,"Sudadera","/imagenes/sudadera.jpg"),
+(450,"Tops","/imagenes/croptopnegro.jpg"),
+(800,"Vestidos","/imagenes/vestidogris.jpg"),
+(1500,"Conjuntos","/imagenes/zaful-mujeres-conjunto-de-dos-piezas-sexy.jpg"),
+(600,"Faldas","/imagenes/polleras.jpg"),
+(1400,"Bottoms","/imagenes/bottoms.jpg"),
+(2000,"Bikinis","/imagenes/bikinis.jpg"),
+(23423,"Hombres","/imagenes/hombres.jpg")
